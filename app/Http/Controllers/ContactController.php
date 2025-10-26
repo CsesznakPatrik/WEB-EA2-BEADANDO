0feccf471e208eb79a0031ef6498c9f45210e17f
CsesznakPatrik <csesznakpatrik@gmail.com>
2025-10-26 12:13:57 +0100

    added form validation + messages database

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Message;
+class ContactController extends Controller
+{
+    public function store(Request $request)
+    {
+        //forms mező ellenőrzése
+        $validatedData = $request->validate([
+            'email' => 'required|string|max:255',
+            'name' => 'required|string|max:255',
+            'subject' => 'required|string|max:255',
+            'message' => 'required|string',
+        ], [
+            'name.required' => 'A név mező kitöltése kötelező',
+            'email.required' => 'Az e-mail cím mező kitöltése kötelező',
+            'email.email' => 'Érvényes e-mail címet adjon meg',
+            'subject.required' => 'A tárgy mező kitöltése kötelező',
+            'message.required' => 'Az üzenet mező kitöltése kötelező',
+        ]);
+
+        //adatbázisba mentés
+        Message::create($validatedData);
+
+        return redirect('/');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
+use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
 });
 
-
+Route::post('kapcsolat-kuldes', [ContactController::class, 'store'])->name('contact.store');
